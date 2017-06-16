e16ffccd37846f03124154cdf30b3291520d888e
Douglas Balkin <dbalkin77@gmail.com>
2017-06-15 22:19:00 -0500

    links to external libraries set

---
+++ assets/javascript/logic.js
@@ -0,0 +1,14 @@
+console.log('this is working');
+
+var config = {
+    apiKey: "AIzaSyCw8CLQDvv114eFr05CJa605IZ3ZsT2wMo",
+    authDomain: "dbalkin77db.firebaseapp.com",
+    databaseURL: "https://dbalkin77db.firebaseio.com",
+    projectId: "dbalkin77db",
+    storageBucket: "dbalkin77db.appspot.com",
+    messagingSenderId: "906019419833"
+};
+// Initialize the database
+firebase.initializeApp(config);
+// variable that is a reference to the database
+var database = firebase.database();
