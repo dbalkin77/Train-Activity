0366f55c9d0093009233ab96287d8183fc0ec9bd
Douglas Balkin <dbalkin77@gmail.com>
2017-06-18 15:05:21 -0500

    updates to moment.js logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,15 +25,44 @@ $("#button").on("click", function () {
 
  trainName = $("#train-name").val().trim();
  trainDestination = $("#train-destination").val().trim();
- firstTrainTime = $("#first-train-time").val().trim();
  trainFrequency = $("#train-frequency").val().trim();
 
 	console.log(trainName);
 	console.log(trainDestination);
-	console.log(firstTrainTime);
 	console.log(trainFrequency);
 
-// Code for push
+	// Convert first-train-time for browser outputs
+	firstTrainTime = $("#first-train-time").val().trim();
+	console.log(firstTrainTime);
+
+	var firstTrainTimeConverted = moment(firstTrainTime, "HH:mm:").subtract(1, "year");
+	console.log(firstTrainTimeConverted);
+
+	// Current Time
+	var currentTime = moment();
+	console.log ("Current time is: " + moment(currentTime).format("HH:mm"));
+
+	// Difference in Train Times
+	var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+	console.log("Difference in time is " + diffTime);
+
+	// Time apart
+	var trainRemainder = diffTime % trainFrequency;
+	console.log(trainRemainder);
+
+	// Minutes Until Train
+	var minutesUntilTrain = trainFrequency - trainRemainder;
+	console.log("Minutes until next train: " + minutesUntilTrain);
+
+	// Next Arrival
+	var nextArrival = moment().add(minutesUntilTrain, "minutes");
+	console.log("Next train arrives at: " + moment(nextArrival).format("HH:mm"));
+
+	$("#next-arrival-output").append("<tr>" + "<td>" + moment(nextArrival).format("HH:mm") + "</td>" + "</tr>");
+	$("#minutes-away-output").append("<tr>" + "<td>" + minutesUntilTrain + "</td>" + "</tr>");
+	
+
+	// Code for push
 	database.ref().push({
 
 		trainName: trainName,
@@ -47,7 +76,7 @@ $("#button").on("click", function () {
 database.ref().on("child_added", function (childSnapshot){
 	console.log(childSnapshot.val().trainName);
 	console.log(childSnapshot.val().trainDestination);
-	console.log(childSnapshot.val().firstTrainTime);
+	// console.log(childSnapshot.val().firstTrainTime);
 	console.log(childSnapshot.val().trainFrequency);
 
 // Add output to table
@@ -55,8 +84,15 @@ database.ref().on("child_added", function (childSnapshot){
 $("#name-output").append("<tr>" + "<td>" + childSnapshot.val().trainName + "</td>" + "</tr>");
 $("#destination-output").append("<tr>" + "<td>" + childSnapshot.val().trainDestination + "</td>" + "</tr>");
 $("#frequency-output").append("<tr>" + "<td>" + childSnapshot.val().trainFrequency + "</td>" + "</tr>");
-// $("#name-output").append("<tr>" + "<td>" + childSnapshot.val().trainFrequency + "</td>" + "</tr>");
+
+$("#name-input").text("");
+ // trainDestination = $("#train-destination").val().trim();
+ // trainFrequency = $("#train-frequency").val().trim();
 
 }); 
+
+// Caluclating Train times:
+
+
 
 // 
