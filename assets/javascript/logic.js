369db26cf1e6d804761e6b044c33c79a84a352b5
Douglas Balkin <dbalkin77@gmail.com>
2017-06-18 13:24:05 -0500

    additons to t to table logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,6 @@
 // Variables ////////////////////////////////////////////////////
 
+
 var config = {
     apiKey: "AIzaSyCw8CLQDvv114eFr05CJa605IZ3ZsT2wMo",
     authDomain: "dbalkin77db.firebaseapp.com",
@@ -12,30 +13,50 @@ var config = {
 firebase.initializeApp(config);
 // variable that is a reference to the database
 var database = firebase.database();
+// Variables to store user input
+var trainName = "";
+var trainDestination = "";
+var firstTrainTime = 0;
+var trainFrequency = 0;
 
 // Functions ///////////////////////////////////////////////////
 $("#button").on("click", function () {
+	event.preventDefault();
 
-var trainName = $("#train-name").val().trim();
-var trainDestination = $("#train-destination").val().trim();
-var firstTrainTime = $("#first-train-time").val().trim();
-var trainFrequency = $("#train-frequency").val().trim();
+ trainName = $("#train-name").val().trim();
+ trainDestination = $("#train-destination").val().trim();
+ firstTrainTime = $("#first-train-time").val().trim();
+ trainFrequency = $("#train-frequency").val().trim();
 
 	console.log(trainName);
 	console.log(trainDestination);
 	console.log(firstTrainTime);
 	console.log(trainFrequency);
 
-	database.ref().set({
+// Code for push
+	database.ref().push({
+
 		trainName: trainName,
 		trainDestination: trainDestination,
 		firstTrainTime: firstTrainTime,
-		trainFrequency: trainFrequency
+		trainFrequency: trainFrequency,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP,
 	});
 });
 
-database.ref().on("value", function (snapshot){
-	console.log(snapshot.val());
+database.ref().on("child_added", function (childSnapshot){
+	console.log(childSnapshot.val().trainName);
+	console.log(childSnapshot.val().trainDestination);
+	console.log(childSnapshot.val().firstTrainTime);
+	console.log(childSnapshot.val().trainFrequency);
+
+// Add output to table
+
+$("#name-output").append("<tr>" + "<td>" + childSnapshot.val().trainName + "</td>" + "</tr>");
+$("#destination-output").append("<tr>" + "<td>" + childSnapshot.val().trainDestination + "</td>" + "</tr>");
+$("#frequency-output").append("<tr>" + "<td>" + childSnapshot.val().trainFrequency + "</td>" + "</tr>");
+// $("#name-output").append("<tr>" + "<td>" + childSnapshot.val().trainFrequency + "</td>" + "</tr>");
+
 }); 
 
 // 
